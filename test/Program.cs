49e137065c41b3cd63f2496eccf40bdafe193949
PapaIPapa <131011950+PapaIPapa@users.noreply.github.com>
2025-05-25 14:40:41 +0300

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string str = "asdsgfdhdjjdjhsg";
+int Int = 101;
+double dou = 4.5;
+bool Bool = true;
+
+
+//if (Int > 100)
+//{
+//    dou = 10.5;
+//}
+//else 
+//{
+//    Bool = false;
+//}
+
+//Console.WriteLine(dou);
+//Console.WriteLine(Bool);
+
+static void Hello()
+{
+    Console.WriteLine("Hello Method!");
+}
+
+
+
+
+
+
+
+
+
+Hello();
